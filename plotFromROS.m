a7de3c772d25ede12dcb894c3d68ba344aee92c9
Luigi Catello <62564969+louis1642@users.noreply.github.com>
2023-11-25 18:30:38 +0100

    MATLAB, part 2

--- plotFromROS.m
+++ plotFromROS.m
@@ -2,19 +2,37 @@ close all
 clear
 clc
 
-% load ROS bag
-bag = rosbag("data_2023-11-24-13-35-49.bag");
+plotErrorAndTorques("data_2023-11-24-13-35-49.bag");
 
-% error plotting
-err = bag.select("Topic","/iiwa/error");
-errTs = timeseries(err);
-plot(errTs)
 
-% torques
-for i = 1:7
-    topicName = "/iiwa/iiwa_joint_" + string(i) + "_effort_controller/command";
-    torqueTs = timeseries(bag.select("Topic", topicName));
-    figure
-    plot(torqueTs), hold on
+
+function plotErrorAndTorques(bagname)
+    % load ROS bag
+    bag = rosbag(bagname);
+    
+    % error plotting
+    err = bag.select("Topic","/iiwa/error");
+    errTs = timeseries(err);
+    figure("Name","Error")
+    plot(errTs)
+    grid on
+    ylabel("")
+    title("Error norm")
+    
+    % torques
+    fig_torques = figure("Name","Torques");
+    leg = cell(7,1);
+    for i = 1:7
+        topicName = "/iiwa/iiwa_joint_" + string(i) + "_effort_controller/command";
+        torqueTs = timeseries(bag.select("Topic", topicName));
+        figure(fig_torques);
+        plot(torqueTs)
+        hold on
+        leg{i} = "Joint " + i + " torque";
+    end
+    hold off
+    title("Torques")
+    legend(leg,"location","sw")
+    ylabel("")
+    grid on
 end
-hold off
